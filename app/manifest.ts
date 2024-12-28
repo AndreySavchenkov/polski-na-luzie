4154ffe6b941cedc110b02afddb1be97614172eb
AndreySavchenkov <sannndvik16@gmail.com>
2024-12-28 09:14:31 +0100

    feat: added icons for PWA

--- app/manifest.ts
+++ app/manifest.ts
@@ -11,12 +11,12 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: '#000000',
     icons: [
       {
-        src: '/public/logoWithText.png',
+        src: '/logoWithText192.png',
         sizes: '192x192',
         type: 'image/png',
       },
       {
-        src: '/public/logoWithText.png',
+        src: '/logoWithText512.png',
         sizes: '512x512',
         type: 'image/png',
       },
