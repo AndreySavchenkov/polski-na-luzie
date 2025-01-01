8a0f17a7c0af2aa5b495e51a1e8642f1c065b889
AndreySavchenkov <sannndvik16@gmail.com>
2025-01-01 12:18:07 +0100

    refactor: remove prompt for account selection in Google authentication options; streamline NextAuth configuration for improved simplicity and clarity

--- app/api/auth/[...nextauth]/auth.ts
+++ app/api/auth/[...nextauth]/auth.ts
@@ -12,7 +12,6 @@ export const authOptions: NextAuthOptions = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
       authorization: {
         params: {
-          prompt: "select_account",
           access_type: "offline",
           response_type: "code",
           include_granted_scopes: true,
